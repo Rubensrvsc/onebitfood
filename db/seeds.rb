b04517120d974e6392d9e3eddb145bad52a6f7cf
rubensrvsc <rubensspfc100@gmail.com>
2020-04-21 15:43:04 -0300

    cadastrando as categorias de comida no seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+ProductCategory.destroy_all
+Restaurant.destroy_all
+Category.destroy_all
+
+puts 'Criando Categorias'
+
+path_image = 'public/images/categories/mexican.jpg'
+c = Category.create(id: 1, title: 'mexicana')
+c.image.attach(io: File.open(path_image), filename: 'mexican.jpg')
+
+path_image = 'public/images/categories/italian.jpg'
+c = Category.create(id: 2, title: 'italiana')
+c.image.attach(io: File.open(path_image), filename: 'italian.jpg')
+
+path_image = 'public/images/categories/japonese.jpg'
+c = Category.create(id: 3, title: 'japonesa')
+c.image.attach(io: File.open(path_image), filename: 'japonese.jpg')
+
+path_image = 'public/images/categories/vegan.jpg'
+c = Category.create(id: 4, title: 'vegana')
+c.image.attach(io: File.open(path_image), filename: 'vegan.jpg')
+
+path_image = 'public/images/categories/peruvian.jpg'
+c = Category.create(id: 5, title: 'peruana')
+c.image.attach(io: File.open(path_image), filename: 'peruvian.jpg')
